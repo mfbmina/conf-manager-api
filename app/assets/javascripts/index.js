6f4a7303984f6acd03f981906435009904926144
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-04-20 17:11:08 +0100

    Initial theme scaffolding (#10)

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require ./vendor/lib/jquery/jquery.min
+//= require ./vendor/lib/bootstrap/js/popper.min
+//= require ./vendor/lib/bootstrap/js/bootstrap.min
+//= require ./vendor/jquery.slimscroll
+//= require ./vendor/sidebarmenu
+//= require ./vendor/lib/sticky-kit-master/dist/sticky-kit.min
+//= require ./vendor/lib/morris-chart/raphael-min
+//= require ./vendor/lib/morris-chart/morris
+//= require ./vendor/lib/morris-chart/dashboard1-init
+//= require ./vendor/lib/calendar-2/moment.latest.min
+//= require ./vendor/lib/calendar-2/semantic.ui.min
+//= require ./vendor/lib/calendar-2/prism.min
+//= require ./vendor/lib/calendar-2/pignose.calendar.min
+//= require ./vendor/lib/calendar-2/pignose.init
+//= require ./vendor/lib/owl-carousel/owl.carousel.min
+//= require ./vendor/lib/owl-carousel/owl.carousel-init
+//= require ./vendor/scripts
+//= require ./vendor/custom.min
